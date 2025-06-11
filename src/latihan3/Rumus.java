3e212fcbaaeb682a99377cedf17b4070f5223083
Fukaziroh <rm26277@gmail.com>
2025-06-11 15:55:17 +0800

    membuat mutator

--- src/latihan3/Rumus.java
+++ src/latihan3/Rumus.java
@@ -11,4 +11,27 @@ public class Rumus {
     int panjang;
     int lebar;
     int hasil;
+
+    // Konstruktor
+    public Rumus() {
+        this.sisi = 0;
+        this.panjang = 0;
+        this.lebar = 0;
+        this.hasil = 0;
+    }
+    
+    // Method menghitung luas persegi
+    public void hitungLuasPersegi() {
+        this.hasil = this.sisi * this.sisi;
+    }
+    
+    // Method menghitung luas persegi panjang
+    public void hitungLuasPersegiPanjang() {
+        this.hasil = this.panjang * this.lebar;
+    }
+    
+    // Accessor untuk mendapatkan nilai hasil
+    public int getHasil() {
+        return this.hasil;
+    }
 }
