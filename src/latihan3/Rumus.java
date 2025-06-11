4ec8c2690cc675f83b4b8b4a6d7743d235f4f9f8
Fukaziroh <rm26277@gmail.com>
2025-06-11 15:48:45 +0800

    first commit

--- src/latihan3/Rumus.java
+++ src/latihan3/Rumus.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package latihan3;
 
 /**
- *
- * @author USER
+ * Nama: [Nama Anda]
+ * NPM: [NPM Anda]
+ * Kelas: [Kelas Anda]
  */
 public class Rumus {
     
