7e99ea0f0ba974639bf281b60c3c1856d914063f
Fukaziroh <rm26277@gmail.com>
2025-06-11 16:04:40 +0800

    Membarui

--- src/latihan3/RumusBeraksi.java
+++ src/latihan3/RumusBeraksi.java
@@ -3,17 +3,17 @@ package latihan3;
 public class RumusBeraksi {
     public static void main(String[] args) {
         // Membuat objek dengan nama panggilan saya
-        Rumus alex = new Rumus();
+        Rumus rehan = new Rumus();
         
         // Menghitung luas persegi
-        alex.sisi = 5; // set nilai sisi
-        alex.hitungLuasPersegi();
-        System.out.println("Luas Persegi dengan sisi " + alex.sisi + " = " + alex.getHasil());
+        rehan.sisi = 5; // set nilai sisi
+        rehan.hitungLuasPersegi();
+        System.out.println("Luas Persegi dengan sisi " + rehan.sisi + " = " + rehan.getHasil());
         
         // Menghitung luas persegi panjang
-        alex.panjang = 8; // set nilai panjang
-        alex.lebar = 3;   // set nilai lebar
-        alex.hitungLuasPersegiPanjang();
-        System.out.println("Luas Persegi Panjang " + alex.panjang + "x" + alex.lebar + " = " + alex.getHasil());
+        rehan.panjang = 8; // set nilai panjang
+        rehan.lebar = 3;   // set nilai lebar
+        rehan.hitungLuasPersegiPanjang();
+        System.out.println("Luas Persegi Panjang " + rehan.panjang + "x" + rehan.lebar + " = " + rehan.getHasil());
     }
 }
